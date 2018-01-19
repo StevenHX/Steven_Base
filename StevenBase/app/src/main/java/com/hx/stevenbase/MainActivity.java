b67c3c879738d6c264fc454ef9a4e39647589385
hx <datongganjin@163.com>
2018-01-19 17:31:22 +0800

    selectDialog

--- StevenBase/app/src/main/java/com/hx/stevenbase/MainActivity.java
+++ StevenBase/app/src/main/java/com/hx/stevenbase/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hx.stevenbase;
 
 import android.Manifest;
+import android.graphics.Color;
 import android.view.View;
 import android.widget.Button;
 import com.hx.steven.activity.BaseActivity;
@@ -11,7 +12,12 @@ import com.hx.steven.util.MPermissionUtil;
 import com.hx.steven.util.ToastUtil;
 import com.hx.stevenbase.ui.Login.LoginActivity;
 import com.hx.stevenbase.ui.Set.SetActivity;
+import com.wevey.selector.dialog.DialogOnClickListener;
+import com.wevey.selector.dialog.MDAlertDialog;
+import com.wevey.selector.dialog.NormalAlertDialog;
+import com.wevey.selector.dialog.NormalSelectionDialog;
 
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -27,8 +33,39 @@ public class MainActivity extends BaseActivity {
             @Override
             public void onClick(View view) {
 //                launch(MainActivity.this, LoginActivity.class);
-                launch(MainActivity.this, SetActivity.class);
-                ToastUtil.showCustomToast(MainActivity.this,"2333333");
+//                launch(MainActivity.this, SetActivity.class);
+//                ToastUtil.showCustomToast(MainActivity.this,"2333333");
+
+//                new MDAlertDialog.Builder(MainActivity.this)
+//                        .setContentText("加载中...")
+//                        .setContentTextSize(30)
+//                        .setContentTextColor(R.color.base_deep)
+//                        .setOnclickListener(new DialogOnClickListener() {
+//                            @Override
+//                            public void clickLeftButton(View view) {
+//
+//                            }
+//
+//                            @Override
+//                            public void clickRightButton(View view) {
+//
+//                            }
+//                        })
+//
+//                        .build()
+//                        .show();
+                NormalSelectionDialog selectionDialog =  new NormalSelectionDialog.Builder(MainActivity.this)
+                        .setTitleText("消息")
+                        .setlTitleVisible(true)
+                        .setTitleTextColor(R.color.base_deep)
+                        .build();
+                ArrayList<String> data = new ArrayList<String>();
+                data.add("1111");
+                data.add("2222");
+                data.add("3333");
+                data.add("4444");
+                 selectionDialog.setDataList(data);
+                 selectionDialog.show();
             }
         });
         LogUtil.e("11111111");
@@ -36,6 +73,8 @@ public class MainActivity extends BaseActivity {
         hideLeftIcon();
         circleImageView = (CircleImageView) findViewById(R.id.circleImage);
         circleImageView.setImageResource(R.drawable.hugh);
+
+
     }
 
     @Override
--- StevenBase/app/src/main/java/com/hx/stevenbase/ui/Set/question/questionFragment.java
+++ StevenBase/app/src/main/java/com/hx/stevenbase/ui/Set/question/questionFragment.java
@@ -1,17 +1,34 @@
 package com.hx.stevenbase.ui.Set.question;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+
 import com.hx.steven.fragment.BaseFragment;
 import com.hx.stevenbase.R;
+import com.lcodecore.tkrefreshlayout.TwinklingRefreshLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class questionFragment extends BaseFragment {
 
+    @BindView(R.id.recycler_question)
+    RecyclerView recyclerQuestion;
+    @BindView(R.id.refreshLayout_question)
+    TwinklingRefreshLayout refreshLayoutQuestion;
+    Unbinder unbinder;
+
     @Override
     protected void initView(View view) {
+        unbinder = ButterKnife.bind(this, view);
 
     }
 
@@ -20,4 +37,10 @@ public class questionFragment extends BaseFragment {
         return R.layout.fragment_question;
     }
 
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
