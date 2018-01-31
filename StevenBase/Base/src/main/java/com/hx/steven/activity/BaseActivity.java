eeb96537bee9c1588b909510f3b13300ec536c25
hx <datongganjin@163.com>
2018-01-31 13:52:05 +0800

    base修改head高度

--- StevenBase/Base/src/main/java/com/hx/steven/activity/BaseActivity.java
+++ StevenBase/Base/src/main/java/com/hx/steven/activity/BaseActivity.java
@@ -26,7 +26,7 @@ import com.hx.steven.component.MultipleStatusView;
 import com.hx.steven.util.MPermissionUtil;
 import com.hx.steven.util.NetworkUtil;
 
-public abstract class BaseActivity extends AppCompatActivity implements NetworkChangedReceiver.NetEvevt{
+public abstract class BaseActivity extends AppCompatActivity{
 
     private  static  final int TOP_HEIGHT = 48;
     private boolean isShowHeader = true;//是否显示导航栏(默认显示)
@@ -40,7 +40,6 @@ public abstract class BaseActivity extends AppCompatActivity implements NetworkC
 
     private MultipleStatusView multipleStatusView;//内容多状态视图
     private MProgressDialog mProgressDialog;//dialog
-    private NetworkChangedReceiver networkChangedReceiver;//网络状态广播接受者
     public static NetworkChangedReceiver.NetEvevt evevt;//网络状态接口对象
 
     @Override
@@ -51,19 +50,11 @@ public abstract class BaseActivity extends AppCompatActivity implements NetworkC
         initContainer();
         setStatusColor(0x20000000);
         initView();
-
-//        注册网络变化的广播
-//        evevt = this;
-//        networkChangedReceiver = new NetworkChangedReceiver();
-//        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
-//        registerReceiver(networkChangedReceiver, intentFilter);
-
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-//        if(networkChangedReceiver!=null) unregisterReceiver(networkChangedReceiver);
     }
 
     protected abstract void initView();
@@ -92,13 +83,10 @@ public abstract class BaseActivity extends AppCompatActivity implements NetworkC
         View layout = null;
         LayoutInflater inflater = LayoutInflater.from(BaseActivity.this);
         layout = inflater.inflate(getContentId(),null);
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        layout.setLayoutParams(params);
         multipleStatusView.addView(layout);
-        ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams)layout.getLayoutParams();
-        if(isShowHeader){
-            layoutParams.setMargins(0,TOP_HEIGHT,0,0);
-        }else{
-            layoutParams.setMargins(0,0,0,0);
-        }
+
         if(isShowHeader){
             mHeaderLayout = (RelativeLayout) findViewById(R.id.header_view);
             mHeaderLayout.setVisibility(View.VISIBLE);
@@ -192,32 +180,11 @@ public abstract class BaseActivity extends AppCompatActivity implements NetworkC
             lp.topMargin = 0;
             isShowHeader = false;
         } else {
-            lp.topMargin = TOP_HEIGHT;
             isShowHeader = true;
         }
         mHeaderLayout.setVisibility(visibility);
     }
 
-    /**
-     * 实现网络连接变换的方法
-     * @param netMobile
-     */
-    @Override
-    public void onNetChange(int netMobile) {
-        switch (netMobile) {
-            case NetworkUtil.TYPE_NONE:
-                multipleStatusView.showNoNetwork();
-                break;
-            case NetworkUtil.TYPE_MOBILE:
-                multipleStatusView.showContent();
-                break;
-            case NetworkUtil.TYPE_WIFI:
-                multipleStatusView.showContent();
-                break;
-            default:
-                break;
-        }
-    }
     /**
      * 显示dialog
      */
