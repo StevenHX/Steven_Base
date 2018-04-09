4f198de9fa8f94f738c484e8208bcac987732da3
huangxiao <1812247149@qq.com>
2018-04-09 18:46:46 +0800

    mvp添加model(doing)

--- StevenBase/Base/src/main/java/com/hx/steven/Mvp/BaseMvpPresenter.java
+++ StevenBase/Base/src/main/java/com/hx/steven/Mvp/BaseMvpPresenter.java
@@ -1,45 +1,47 @@
 package com.hx.steven.Mvp;
 
-import com.orhanobut.logger.Logger;
+import java.util.List;
 
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 
 public class BaseMvpPresenter<V extends BaseMvpView> implements Presenter<V> {
     private V mvpView;
+    private List<BaseMvpModel> models;
     private CompositeDisposable composite = new CompositeDisposable();
 
     @Override
     public void attachView(V mvpView) {
-        Logger.d("attachView: ");
         this.mvpView = mvpView;
     }
 
     @Override
     public void detachView() {
-        Logger.d( "detachView: ");
         mvpView = null;
     }
 
     @Override
     public void subscribe(Disposable disposable) {
-        Logger.d("subscribe");
         composite.add(disposable);
     }
 
     @Override
     public void unScribe() {
-        Logger.d("unScribe");
         composite.clear();
     }
 
-    /**
-     * 判断 view是否为空
-     * @return
-     */
-    public  boolean isAttachView(){
-        return mvpView != null;
+    @Override
+    public void attachModels(List<BaseMvpModel> models) {
+        this.models = models;
     }
+
+    @Override
+    public void detachModel() {
+        models.clear();
+        models = null;
+    }
+//===================================与View有关公共方法==========================================
+
     /**
      * 返回目标view
      * @return
@@ -51,7 +53,7 @@ public class BaseMvpPresenter<V extends BaseMvpView> implements Presenter<V> {
      * 检查view和presenter是否连接
      */
     public void checkViewAttach(){
-        if(! isAttachView()){
+        if(mvpView == null){
             throw  new MvpViewNotAttachedException();
         }
     }
@@ -61,6 +63,27 @@ public class BaseMvpPresenter<V extends BaseMvpView> implements Presenter<V> {
     public static   class  MvpViewNotAttachedException extends RuntimeException {
         public  MvpViewNotAttachedException(){
             super("请求数据前请先调用 attachView(MvpView) 方法与View建立连接");
+        }
+    }
+
+//==========================================与model相关公共方法===================================
+    /**
+     * 获取model
+     */
+    public List<BaseMvpModel> getMvpModels(){
+        if(models == null){
+            throw  new MvpViewNotAttachedException();
+        }else{
+            return models;
+        }
+    }
+
+    /**
+     * 自定义异常
+     */
+    public static   class  MvpModelNotAttachedException extends RuntimeException {
+        public  MvpModelNotAttachedException(){
+            super("请求数据前请先调用 attachModels(MvpView) 方法与View建立连接");
         }
     }
 }
--- StevenBase/Base/src/main/java/com/hx/steven/activity/BaseMvpActivity.java
+++ StevenBase/Base/src/main/java/com/hx/steven/activity/BaseMvpActivity.java
@@ -1,11 +1,14 @@
 package com.hx.steven.activity;
 
+import com.hx.steven.Mvp.BaseMvpPresenter;
 import com.hx.steven.Mvp.BaseMvpView;
-import com.hx.steven.Mvp.Presenter;
 import com.hx.steven.util.ToastUtil;
 
-public abstract class BaseMvpActivity<P extends Presenter<V>,V extends BaseMvpView> extends BaseActivity implements BaseMvpView{
+public abstract class BaseMvpActivity<P extends BaseMvpPresenter> extends BaseActivity implements BaseMvpView{
     public P mPresenter;
+
+    public abstract P creatPresenter();
+    public abstract void initMvpView();
 
     @Override
     public void showLoding(String msg) {
@@ -25,16 +28,15 @@ public abstract class BaseMvpActivity<P extends Presenter<V>,V extends BaseMvpVi
     @Override
     protected void initView() {
         mPresenter = creatPresenter();
-        if(mPresenter!=null) mPresenter.attachView((V)this);
+        if(mPresenter != null) mPresenter.attachView(this);
         initMvpView();
     }
 
-    public abstract P creatPresenter();
-    public abstract void initMvpView();
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(mPresenter!=null) {
+        if(mPresenter != null) {
             mPresenter.detachView();
             mPresenter.unScribe();
         }
